10c187d4052f9c0fbe26ba319d01d4b6d7b1a13e
felipesantosamorim1313-ops <felipesantosamorim1313@gmail.com>
2026-04-02 22:07:34 -0300

    modo escuro 1.0

---
+++ js/interatividade.js
@@ -0,0 +1,41 @@
+$(function(){
+
+    //ENTRADA
+
+//descartavel//
+
+    var botoes = $(".botao");
+    var titulo = $("h1");
+    var subtitulo = $("#subtitulo");
+    var icones = $("svg");
+    var fundo = $("body");
+    var botao_modoEscuro = $("#modoEscuro");
+    var modoEscuro = false
+
+    // PROCESSAMENTO
+
+    botao_modoEscuro.click(()=>{
+
+
+    if( modoEscuro == false){
+
+        botoes.css("background-color", "black");
+        botoes.css("color", "white");
+
+        modoEscuro = true
+
+    }else{
+
+        
+        botoes.css("background-color", "white");
+        botoes.css("color", "black");
+
+        modoEscuro = false
+
+    }
+      
+    });
+        
+    // SAIDA
+
+});
