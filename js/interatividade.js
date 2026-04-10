00293156161833e2815b6e94cd866f73148c16d5
felipesantosamorim1313-ops <felipesantosamorim1313@gmail.com>
2026-04-09 21:12:19 -0300

    responsabilidade

--- js/interatividade.js
+++ js/interatividade.js
@@ -24,11 +24,27 @@ $(function(){
 
         modoEscuro = true
 
-    }else{
+        fundo.css("background-image","url('https://i.pinimg.com/736x/43/84/10/438410cdc0128036c97e3b141f7599d2.jpg')")
 
-        
-        botoes.css("background-color", "white");
-        botoes.css("color", "black");
+        titulo.css ("color", "rgb(20, 219, 70)");
+        subtitulo.css ("color", "rgb(128, 238, 84)");
+        icones.css ("fill", "rgb(20, 236, 74)");
+           
+ 
+        modoEscuro = true;
+ 
+ 
+    }else{
+ 
+        botoes.css ("background-color", "rgb(235, 7, 45)");
+        botoes.css ("color", "black");
+ 
+ 
+       fundo.css ("background-image", "url('https://i.pinimg.com/736x/ca/f4/44/caf44400c1eae35f0caadadc652a10cc.jpg')");
+ 
+        titulo.css ("color", "white");
+        subtitulo.css ("color", "white");
+        icones.css ("fill", "red");
 
         modoEscuro = false
 
